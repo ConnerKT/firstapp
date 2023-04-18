47d93edea29cc48559c5a7840caeefe9b2843a94
ConnerKT <connerthompson121@gmail.com>
2023-04-18 15:23:06 -0500

    hornedbeast solution

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function HornedBeast() {
+function HornedBeast(props) {
   return (
     <div>
         {/* This includes the Name of the beast, its alt,src,and title, and the description*/}
-        <h2> </h2>
-        <img alt='' src='' title=''></img>
-        <p></p>
+        <h2>{props.title}</h2>
+        <img alt={props.alt} src={props.imageUrl} title={props.title}></img>
+        <p>{props.description}</p>
     </div>
   
   );
